c65f3dad7f44d7c975f7dc217d8f13d96eccde97
summerfr33ze <trevorjamesmurphy@gmail.com>
2023-06-12 00:30:37 -0400

    almost done with boxer responses

---
+++ script.js
@@ -0,0 +1,91 @@
+const boxersList = document.querySelector("#boxers")
+const sealersList = document.querySelector("#sealers")
+const boxers = [
+    {name: "Jimmy",
+    bad_responses: ["Jimmy still doesn't understand that 2511s and 2611s are two different things", "Jimmy forgot to label 4 of his 5907s on the top. Beecher lectured him for a full minute. His window filled up and he never recovered", "Jimmy got help from Trevor with his DFUs. This was a mistake because he ended up with an angio inside of an angio and a random extra device. Kit was 100 percented for DFUs" ],
+    good_responsesa: ["Jimmy finally found out that the 'NC' in 'NCMR' stands for 'non-conformance' and he should conform to proper manufacturing standards. His kit was flawless as a result", "Jimmy actually finished his fries today and consumed enough calories to complete his kit without error" ],
+    heat_level: 3,
+    },
+    {name: "Bill",
+    bad_responses: ["Bill was distracted by the beauty of the women on his line and put 26 pouches in a line kit inner.", "Bill found out that HR didn't want him to use the 'r' word and used it anyway. Pretty (re)tardiff.", "Bill stole a squeeze contrast controller from the kit to use as a sex toy. Emergency surgery was required"],
+    good_responses: [""],
+    heat_level: 4,
+    },
+    {name: "Harry",
+    bad_responses: ["Harry made a crazy joke at Town Hall and got called down to HR. He was already missing three DFUs. Bill screwed up his count while he was gone","Harry spent his whole kit browsing the internet and ended up boxing up his pens and box cutters instead of pouches"],
+    good_responses: ["Harry was put on the Japan line and made good use of his scale, weighing every box. His count was flawless all day.", "Harry was put on a line with one of his daughters. Van Guilder energy pervaded the custom packaging department and everyone was magically on count"],
+    heat_level: 10,
+    },
+    {name: "Heidi",
+    bad_responses: ["Heidi used 96 bags instead of 13s and we had to 100 percent a three pallet connect"],
+    good_responses: ["Kurt helped Heidi spot every trace of foreign matter in her kit"]
+    },
+    {name: "Jon",
+    bad_responses: ["Jon was busy thinking about sexy vampires and put the wrong number of pouches in every box", "Jon left his line to do three audits and Bill had to bail him out.", "Jon went to the bathroom and Trevor screwed up Jon's count while he was gone"],
+    good_responses: ["Jon used his 'eagle eyes' power-up to spot every trace of foreign matter in the kit.", "Jon argued with Kurt for 20 minutes about what the best guitar hero song is but the count was on somehow"]
+    },
+    {name: "Kurt",
+    bad_responses: ["Kurt laughed for so long he had trouble breathing and he wound up in the hospital. Medline products saved his life", "Kurt forgot a stop label and Trevor missed it on the audit.","There were so many wrinkled seals in Kurt's audit box that someone contacted PETA"],
+    good_responses: ["Kurt was deep in a discussion about his favorite band 'Avenged Sevenfold' when he thought about the fact that he would be NCMR'd 'sevenfold' if he wasn't careful. His audit box was good", "Kurt distracted Aiden with a diatribe on whether Clerks 1 or Clerks 2 was the superior movie and Aiden missed a cheeto crumb in Kurt's audit box"]
+    },
+    {name: "Kevin",
+    bad_responses: ["Kevin got a contact high from all the weed at a Dave Matthews concert the day before and he was still so stoned that he missed a hair in the seal", "Kevin  seemingly didn't realize there were other jobs out there where he wouldn't have to fix other people's messes like some kind of weird box janitor. He continued boxing his kit."],
+    good_responses: ["Kevin's been at medline so long he noticed that one of the lines was half an inch longer than its supposed to be. Thanks Rainman.",   ]
+    },
+
+    {name: "Trevor", 
+    bad_responses: ["Trevor had to pee three times in a single kit after drinking a liter of Diet Mountain Lion at lunch. Jon took over his kit while he was gone and his own kit suffered as a result. Kevin and Bill jumped onto Jon's kit. As everyone was boxing each other's kit, Harry had a fire on line 14.", "Justin brought a microscope out to final box and QDA'd Trevor for a dead skin cell"],
+    good_responses: ["Trevor used his stager's wisdom to notice a subtle manufacturing defect in the angio sacs. The writing said 'Angio Shaq' for some reason and they were three times the size"]
+    
+    },
+    {name: "Aiden",
+    bad_responses: ["Aiden was tired of Bill calling him Aladdin. He decided to use reverse psychology He leaned into the stereotype by growing out his facial hair and carrying his box cutters in a genie bottle. HR fired him for promoting racist depictions of middle easterners."]
+}
+    
+]
+const sealers = [
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    }
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+    {name: "",
+    responses: 
+    
+    },
+]
